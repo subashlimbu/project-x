192338cbce90bcd159a667b65bd4caa73230c643
subashlimbu <prajan_l@hotmail.com>
2020-05-31 08:58:33 +0100

    postgresql

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import django_heroku
+import psycopg2
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
